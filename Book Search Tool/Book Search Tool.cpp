1b9f00de93095ae64055bab6d70b63eb352125d3
16213-beep <16213@ait.nsw.edu.au>
2026-04-11 12:06:25 +0000

    changing comments

--- Book Search Tool/Book Search Tool.cpp
+++ Book Search Tool/Book Search Tool.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main() {
     Book library[5];
 
-    // Initialise books
+   
     library[0].setBookDetails("Book A", "Author A", "101", true, "2024");
     library[1].setBookDetails("Book B", "Author B", "105", true, "2023");
     library[2].setBookDetails("Book C", "Author C", "102", true, "2022");
     library[3].setBookDetails("Book D", "Author D", "104", true, "2021");
     library[4].setBookDetails("Book E", "Author E", "103", true, "2020");
 
-    // Sort books
+    
     Book::sortBookData(library, 5);
 
-    // Display books
+    
     cout << "Library Books:\n";
     for (int i = 0; i < 5; i++) {
         library[i].displayBookDetails();
